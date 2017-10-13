06f8395d2a55703869e61a9fdcef931fa3e5d285
Zeljko Cvijanovic <cvzeljko@gmail.com>
2017-10-13 21:09:12 +0200

    ADS1115

--- src/ADS1115.pas
+++ src/ADS1115.pas
@@ -1,3 +1,8 @@
+{**********************************************************
+*    Copyright (c) Zeljko Cvijanovic Teslic RS/BiH
+*    www.zeljus.com
+*    Created by: 13-10-2017
+***********************************************************}
 unit ADS1115;
 
 {$mode objfpc}{$H+}
@@ -40,35 +45,14 @@ const
    ADS1015_REG_CONFIG_OS_NOTBUSY   = $8000;  // Read: Bit = 1 when device is not performing a conversion
 
    ADS1015_REG_CONFIG_MUX_MASK     = $7000;
-   ADS1015_REG_CONFIG_MUX_DIFF_0_1 = $0000;  // Differential P = AIN0, N = AIN1 (default)
-   ADS1015_REG_CONFIG_MUX_DIFF_0_3 = $1000;  // Differential P = AIN0, N = AIN3
-   ADS1015_REG_CONFIG_MUX_DIFF_1_3 = $2000;  // Differential P = AIN1, N = AIN3
-   ADS1015_REG_CONFIG_MUX_DIFF_2_3 = $3000;  // Differential P = AIN2, N = AIN3
-   ADS1015_REG_CONFIG_MUX_SINGLE_0 = $4000;  // Single-ended AIN0
-   ADS1015_REG_CONFIG_MUX_SINGLE_1 = $5000;  // Single-ended AIN1
-   ADS1015_REG_CONFIG_MUX_SINGLE_2 = $6000;  // Single-ended AIN2
-   ADS1015_REG_CONFIG_MUX_SINGLE_3 = $7000;  // Single-ended AIN3
 
    ADS1015_REG_CONFIG_PGA_MASK     = $0E00;
-   ADS1015_REG_CONFIG_PGA_6_144V   = $0000;  // +/-6.144V range = Gain 2/3
-   ADS1015_REG_CONFIG_PGA_4_096V   = $0200;  // +/-4.096V range = Gain 1
-   ADS1015_REG_CONFIG_PGA_2_048V   = $0400;  // +/-2.048V range = Gain 2 (default)
-   ADS1015_REG_CONFIG_PGA_1_024V   = $0600;  // +/-1.024V range = Gain 4
-   ADS1015_REG_CONFIG_PGA_0_512V   = $0800;  // +/-0.512V range = Gain 8
-   ADS1015_REG_CONFIG_PGA_0_256V   = $0A00;  // +/-0.256V range = Gain 16
 
    ADS1015_REG_CONFIG_MODE_MASK    = $0100;
    ADS1015_REG_CONFIG_MODE_CONTIN  = $0000;  // Continuous conversion mode
    ADS1015_REG_CONFIG_MODE_SINGLE  = $0100;  // Power-down single-shot mode (default)
 
    ADS1015_REG_CONFIG_DR_MASK      = $00E0;
-   ADS1015_REG_CONFIG_DR_128SPS    = $0000;  // 128 samples per second
-   ADS1015_REG_CONFIG_DR_250SPS    = $0020;  // 250 samples per second
-   ADS1015_REG_CONFIG_DR_490SPS    = $0040;  // 490 samples per second
-   ADS1015_REG_CONFIG_DR_920SPS    = $0060;  // 920 samples per second
-   ADS1015_REG_CONFIG_DR_1600SPS   = $0080;  // 1600 samples per second (default)
-   ADS1015_REG_CONFIG_DR_2400SPS   = $00A0;  // 2400 samples per second
-   ADS1015_REG_CONFIG_DR_3300SPS   = $00C0;  // 3300 samples per second
 
    ADS1015_REG_CONFIG_CMODE_MASK   = $0010;
    ADS1015_REG_CONFIG_CMODE_TRAD   = $0000;  // Traditional comparator with hysteresis (default)
@@ -91,18 +75,41 @@ const
 
 
 type
-  adsGain = ( GAIN_TWOTHIRDS    = ADS1015_REG_CONFIG_PGA_6_144V,
-              GAIN_ONE          = ADS1015_REG_CONFIG_PGA_4_096V,
-              GAIN_TWO          = ADS1015_REG_CONFIG_PGA_2_048V,
-              GAIN_FOUR         = ADS1015_REG_CONFIG_PGA_1_024V,
-              GAIN_EIGHT        = ADS1015_REG_CONFIG_PGA_0_512V,
-              GAIN_SIXTEEN      = ADS1015_REG_CONFIG_PGA_0_256V);
+  TGains = ( gaTWOTHIRDS    = $0000,  // +/-6.144V range = Gain 2/3
+             gaONE          = $0200,  // +/-4.096V range = Gain 1
+             gaTWO          = $0400,  // +/-2.048V range = Gain 2 (default)
+             gaFOUR         = $0600,  // +/-1.024V range = Gain 4
+             gaEIGHT        = $0800,  // +/-0.512V range = Gain 8
+             gaSIXTEEN      = $0A00  // +/-0.256V range = Gain 16
+           );
+
+  TChannels = (chMUX_SINGLE_0 = $4000,  // Single-ended AIN0
+               chMUX_SINGLE_1 = $5000,  // Single-ended AIN1
+               chMUX_SINGLE_2 = $6000,  // Single-ended AIN2
+               chMUX_SINGLE_3 = $7000  // Single-ended AIN3
+              );
+
+  TDifs     = (diMUX_DIFF_0_1 = $0000,  // Differential P = AIN0, N = AIN1 (default)
+               diMUX_DIFF_0_3 = $1000,  // Differential P = AIN0, N = AIN3
+               diMUX_DIFF_1_3 = $2000,  // Differential P = AIN1, N = AIN3
+               diMUX_DIFF_2_3 = $3000  // Differential P = AIN2, N = AIN3
+               );
+
+  TRates = (raDR_128SPS    = $0000,  // 128 samples per second
+            raDR_250SPS    = $0020,  // 250 samples per second
+            raDR_490SPS    = $0040,  // 490 samples per second
+            raDR_920SPS    = $0060,  // 920 samples per second
+            raDR_1600SPS   = $0080,  // 1600 samples per second (default)
+            raDR_2400SPS   = $00A0,  // 2400 samples per second
+            raDR_3300SPS   = $00C0  // 3300 samples per second
+           );
 
   { TADS1115 }
 
   TADS1115 = class
-    Fidev: TI2CLinuxDevice;
-    fGain: adsGain;
+    FI2Cdev: TI2CLinuxDevice;
+    FGain: TGains;
+    FRate: TRates;
     fConversionDelay: word;
     fBitShift: byte;
   private
@@ -110,14 +117,16 @@ type
     function readRegister(aReg: byte): Word;
   public
     constructor Create(aAddress: TI2CAddress; aBusID: Longword);
-    destructor Destroy; virtual;
-    function readADC_SingleEnded(aChannel: byte): Word;
+    destructor Destroy;
+
+    function readADC_SingleEnded(aChannel: TChannels): Word;
     function readADC_Differential_0_1: Word;
     function readADC_Differential_2_3: Word;
-    procedure startComparator_SingleEnded(aChannel: byte; aThreshold: word);
+    procedure startComparator_SingleEnded(aChannel: TChannels; aThreshold: word);
     function getLastConversionResults: word;
 
-    property Gain: adsGain read fGain write fGain;
+    property Gain: TGains read fGain write fGain;
+    property Rate: TRates read FRate write FRate;
   end;
 
 
@@ -128,56 +137,52 @@ uses strutils;
 
 procedure TADS1115.writeRegister(aReg: byte; aValue: word);
 begin
-  Fidev.WriteByte(aReg);
-  Fidev.WriteRegWord(aReg, byte(aValue  Shr 8));
-  Fidev.WriteRegWord(aReg, byte(aValue  and $FF));
+  FI2Cdev.WriteByte(aReg);
+  FI2Cdev.WriteRegWord(aReg, byte(aValue  Shr 8));
+  FI2Cdev.WriteRegWord(aReg, byte(aValue  and $FF));
 end;
 
 function TADS1115.readRegister(aReg: byte): Word;
+var
+  temp: byte;
 begin
-  Fidev.WriteRegWord(aReg, ADS1015_REG_POINTER_CONVERT);
-  Result := ((Fidev.ReadRegWord(aReg) Shl 8) or Fidev.ReadRegWord(aReg));
-  Result := Hex2Dec(Copy(IntToHex(Result, 4), 3,2) + Copy(IntToHex(Result, 4), 1,2));
+  FI2Cdev.WriteRegWord(aReg, ADS1015_REG_POINTER_CONVERT);
+  temp:= FI2Cdev.ReadRegWord(aReg);
+  Result := ((FI2Cdev.ReadRegWord(aReg) Shl 8) or temp);
 end;
 
 constructor TADS1115.Create(aAddress: TI2CAddress; aBusID: Longword);
 begin
-   Fidev := TI2CLinuxDevice.Create(aAddress, aBusID);
+   FI2Cdev := TI2CLinuxDevice.Create(aAddress, aBusID);
    fConversionDelay := ADS1115_CONVERSIONDELAY;
    fBitShift := 0;
-   fGain := GAIN_TWOTHIRDS; // +/- 6.144V range (limited to VDD +0.3V max!)
+   fGain := gaTWOTHIRDS;  // +/- 6.144V range (limited to VDD +0.3V max!)
+   FRate := raDR_1600SPS; // 1600 samples per second (default)
 end;
 
 destructor TADS1115.Destroy;
 begin
-  inherited Destroy;
-  Fidev.Free;
+  FI2Cdev.Free;
 end;
 
-function TADS1115.readADC_SingleEnded(aChannel: byte): Word;
+function TADS1115.readADC_SingleEnded(aChannel: TChannels): Word;
 var
   config : Word;
 begin
- if (aChannel > 3) or (aChannel < 0) then  Result := 0;
 
    // Start with default values
     config := ADS1015_REG_CONFIG_CQUE_NONE    or // Disable the comparator (default val)
               ADS1015_REG_CONFIG_CLAT_NONLAT  or // Non-latching (default val)
               ADS1015_REG_CONFIG_CPOL_ACTVLOW or // Alert/Rdy active low   (default val)
               ADS1015_REG_CONFIG_CMODE_TRAD   or // Traditional comparator (default val)
-              ADS1015_REG_CONFIG_DR_1600SPS   or // 1600 samples per second (default)
+              ord(FRate) or // 1600 samples per second (default)
               ADS1015_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
 
     // Set PGA/voltage range
     config := config or word(fGain);
 
     // Set single-ended input channel
-    case aChannel of
-      0: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_0;
-      1: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_1;
-      2: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_2;
-      3: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_3;
-    end;
+    config := config or ord(aChannel);
 
     // Set 'start single-conversion' bit
     config :=  config or ADS1015_REG_CONFIG_OS_SINGLE;
@@ -190,7 +195,7 @@ begin
 
     // Read the conversion results
     // Shift 12-bit results right 4 bits for the ADS1015
-    Result := readRegister(ADS1015_REG_POINTER_CONVERT)  Shr fBitShift;
+    Result := readRegister(ADS1015_REG_POINTER_CONVERT); //  Shr fBitShift;
 end;
 
 function TADS1115.readADC_Differential_0_1: Word;
@@ -203,14 +208,14 @@ begin
             ADS1015_REG_CONFIG_CLAT_NONLAT  or // Non-latching (default val)
             ADS1015_REG_CONFIG_CPOL_ACTVLOW or // Alert/Rdy active low   (default val)
             ADS1015_REG_CONFIG_CMODE_TRAD   or // Traditional comparator (default val)
-            ADS1015_REG_CONFIG_DR_1600SPS   or // 1600 samples per second (default)
+            ord(FRate)   or // 1600 samples per second (default)
             ADS1015_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
 
   // Set PGA/voltage range
   config := config or word(fGain);
 
   // Set channels
-  config := config or ADS1015_REG_CONFIG_MUX_DIFF_0_1;          // AIN0 = P, AIN1 = N
+  config := config  or ord(TDifs(diMUX_DIFF_0_1));          // AIN0 = P, AIN1 = N
 
   // Set 'start single-conversion' bit
   config := config or ADS1015_REG_CONFIG_OS_SINGLE;
@@ -246,14 +251,14 @@ begin
              ADS1015_REG_CONFIG_CLAT_NONLAT  or // Non-latching (default val)
              ADS1015_REG_CONFIG_CPOL_ACTVLOW or // Alert/Rdy active low   (default val)
              ADS1015_REG_CONFIG_CMODE_TRAD   or // Traditional comparator (default val)
-             ADS1015_REG_CONFIG_DR_1600SPS   or // 1600 samples per second (default)
+             ord(FRate)   or // 1600 samples per second (default)
              ADS1015_REG_CONFIG_MODE_SINGLE ;   // Single-shot mode (default)
 
   // Set PGA/voltage range
   config := config or word(fGain);
 
   // Set channels
-  config := config or ADS1015_REG_CONFIG_MUX_DIFF_2_3;          // AIN2 = P, AIN3 = N
+  config := config or ord(TDifs(diMUX_DIFF_2_3));          // AIN2 = P, AIN3 = N
 
   // Set 'start single-conversion' bit
   config := config or ADS1015_REG_CONFIG_OS_SINGLE;
@@ -280,7 +285,7 @@ begin
 
 end;
 
-procedure TADS1115.startComparator_SingleEnded(aChannel: byte; aThreshold: word);
+procedure TADS1115.startComparator_SingleEnded(aChannel: TChannels;  aThreshold: word);
 var
   config: word;
 begin
@@ -289,7 +294,7 @@ begin
                ADS1015_REG_CONFIG_CLAT_LATCH   or // Latching mode
                ADS1015_REG_CONFIG_CPOL_ACTVLOW or // Alert/Rdy active low   (default val)
                ADS1015_REG_CONFIG_CMODE_TRAD   or // Traditional comparator (default val)
-               ADS1015_REG_CONFIG_DR_1600SPS   or // 1600 samples per second (default)
+               ord(FRate)   or // 1600 samples per second (default)
                ADS1015_REG_CONFIG_MODE_CONTIN  or // Continuous conversion mode
                ADS1015_REG_CONFIG_MODE_CONTIN;   // Continuous conversion mode
 
@@ -297,12 +302,7 @@ begin
     config := config or word(fGain);
 
    // Set single-ended input channel
-   case aChannel of
-     0: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_0;
-     1: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_1;
-     2: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_2;
-     3: config := config or ADS1015_REG_CONFIG_MUX_SINGLE_3;
-   end;
+   config := config or ord(aChannel);
 
   // Set the high threshold register
   // Shift 12-bit results left 4 bits for the ADS1015
@@ -336,4 +336,3 @@ end;
 
 
 end.
-
--- src/fpadc.pas
+++ src/fpadc.pas
@@ -96,4 +96,4 @@ begin
 end;
 
 end.
-
+
